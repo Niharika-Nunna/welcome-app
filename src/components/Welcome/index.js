646ee8aefddbd217e89978583d052d44499457e8
Niharika-Nunna <nunnaniharika@gmail.com>
2023-07-20 14:42:24 +0000

    welcome app solution

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,33 +3,44 @@ import './index.css'
 import {Component} from 'react'
 
 class Welcome extends Component {
+  state = {isSubscribed: false}
 
-    state = {isSubscribed: true}
-
-    changeBtn = () => {
-        const {isSubscribed} = this.state
-        if(isSubscribed === true){
-            this.setState({isSubscribed = false})
-        }
-        else{
-            this.setState({isSubscribed = true})
-        }
+  changeBtn = () => {
+    const {isSubscribed} = this.state
+    if (isSubscribed === true) {
+      this.setState({isSubscribed: false})
+    } else {
+      this.setState({isSubscribed: true})
     }
+  }
 
-    
+  render() {
+    const {isSubscribed} = this.state
 
-    render() {
-        const {isSubscribed} = this.state
-
-        return (
-            <div className = "bg-container">
-                <h1 className = "heading">Welcome</h1>
-                <p className = "description">Thank you! Happy Learning</p>
-                {isSubscribed ? (<button className = "subscribe-btn" type = "button" onClick = {this.changeBtn}>Subscribed</button>):
-                (<button className = "subscribe-btn" type = "button" onClick = {this.changeBtn}>Subscribe</button>)}
-            </div>
-        )
-    }
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        {isSubscribed ? (
+          <button
+            className="subscribe-btn"
+            type="button"
+            onClick={this.changeBtn}
+          >
+            Subscribed
+          </button>
+        ) : (
+          <button
+            className="subscribe-btn"
+            type="button"
+            onClick={this.changeBtn}
+          >
+            Subscribe
+          </button>
+        )}
+      </div>
+    )
+  }
 }
 
 export default Welcome
